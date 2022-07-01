a7cacde3eb6c8de7f8c0865e74e028be12855513
mohanadkandil <mohanadmkandil@gmail.com>
2022-07-01 21:55:55 +0200

    feat: add mongoose modles and mongodb configuration

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const colors = require('colors')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
+const connectDB = require('./config/db')
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
 const app = express()
+
+// Connect to database
+connectDB()
 
 app.use('/graphql', graphqlHTTP({
     schema,
